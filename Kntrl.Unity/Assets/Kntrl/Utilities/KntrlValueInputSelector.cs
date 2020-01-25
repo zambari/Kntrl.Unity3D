6a7a531e8b16bf769fe06d8d9555cb96a8acddd4
zambari <zambari@gmail.com>
2020-01-25 01:12:53 +0100

    valie input selectort

---
+++ Kntrl.Unity/Assets/Kntrl/Utilities/KntrlValueInputSelector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class KntrlValueInputSelector
+{
+
+    public GameObject kntrlvalueSource;
+    private IKntrlValueSource valueSource;
+
+    public float GetValue()
+
+    {
+        if (kntrlvalueSource == null) return 0.5f;
+        if (valueSource == null) valueSource = kntrlvalueSource.GetComponent<IKntrlValueSource>();
+        if (valueSource == null)
+        {
+            kntrlvalueSource = null; return 0.3f;
+        }
+        return valueSource.GetValue();
+    }
+
+
+}
